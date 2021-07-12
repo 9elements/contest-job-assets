4c31c447ebd17590487bfac76180802cb5cba748
llogen <christophlange95@googlemail.com>
2021-07-12 15:26:29 +0200

    Bash script to upload the coreboot binary

---
+++ uploadBinarytoS3.sh
@@ -0,0 +1,14 @@
+# Returns:
+#       0 if successful
+#       1 if it fails
+
+datenow=$(date +%Y_%m_%d_%H:%M:%S)
+path="binaries/${datenow}_coreboot.rom"
+RESPONSE=$(aws s3api put-object \
+            --bucket 9e-contest \
+            --body /tmp/coreboot/build/coreboot.rom \
+            --key $path)
+if [ ${?} -ne 0 ]; then
+    errecho "ERROR: AWS reports put-object operation failed.\n$RESPONSE"
+    return 1
+fi
