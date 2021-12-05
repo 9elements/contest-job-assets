bddda0ddd4cbbea320b91ac8b58566fc1d2ab28a
Christian Walter <christian.walter@9elements.com>
2021-12-05 19:01:53 +0100

    qemuscript: change log destination so that we can run multiple instances
    Signed-off-by: Christian Walter <christian.walter@9elements.com>

--- qemuscript.sh
+++ qemuscript.sh
@@ -1,8 +1,8 @@
-touch /tmp/log_qemu_coreboot
-qemu-system-x86_64 -bios build/coreboot.rom -nographic -serial pipe:/tmp/log_qemu_coreboot &
+touch coreboot_qemu.log
+qemu-system-x86_64 -bios build/coreboot.rom -nographic -serial pipe:coreboot_qemu.log &
 PID=$!
 sleep 30
 kill $PID 
-grep "Jumping" /tmp/log_qemu_coreboot
-rm /tmp/log_qemu_coreboot
+grep "Jumping" coreboot_qemu.log
+rm coreboot_qemu.log
 
